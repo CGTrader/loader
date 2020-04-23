9d11fe7678491f69ee090718b1beb83579e9b116
Vilius Luneckas <vilius.luneckas@gmail.com>
2020-04-23 15:51:27 +0300

    Add axios and gallery button tracker

---
+++ src/components/yeet.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+function yeet(uid, platform, action, token) {
+  const payload = { embed: { content_group: platform, action } }
+  const url = `https://arsenal.cgtrader.com/api/v1/product-viewers/${uid}/track`
+  const headers = { 'Authorization': token }
+
+  axios.post(url, payload, { headers: headers })
+    .catch((e) => console.error(e))
+}
+
+export default yeet
